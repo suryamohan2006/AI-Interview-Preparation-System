d8a4923479c6ee32163bcb0b8495846c5dd74b88
suryamohan2006 <suryamohan23551a4442@gmail.com>
2026-08-01 21:36:05 +0530

    Use MongoDB Atlas for deployment

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = 3000;
 
 // Connect to MongoDB
-mongoose.connect("mongodb://127.0.0.1:27017/AIInterviewDB")
+mongoose.connect(process.env.MONGODB_URL)
 .then(() => console.log("✅ MongoDB connected successfully"))
 .catch(err => console.log("MongoDB Error:", err));
 
